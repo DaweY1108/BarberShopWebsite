a52213ec71e99bbb621d618d8ccc3eacb1e36ccc
DaweY1108 <vargadavidzsolt@gmail.com>
2024-04-15 18:23:43 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html> 
+    <head>
+        <title>Home</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+        <div id="header">
+            <h1>Home</h1>
+        </div>
+        <div id="content">
+            <p>Welcome to the home page</p>
+        </div>
+    </body>
+</html>
