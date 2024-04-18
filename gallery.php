0354794def8f5af47922bfa4aebce1595d7e0de1
DaweY1108 <vargadavidzsolt@gmail.com>
2024-04-18 22:10:46 +0200

    Gallery page done

--- gallery.php
+++ gallery.php
@@ -7,39 +7,79 @@
 <div>
     <div class="container text-dark text-center my-5">
       <hr style="border-width: 1px; border-color: black" class="w-50">
-      <h1>- Galéria -</h1>
+      <h1> Galéria </h1>
       <hr style="border-width: 1px; border-color: black" class="w-50">
-      <h4 class="pt-5">Itt megtalálod az összes felhasználó által feltöltött képet, vagy akár te is tölthetsz fel sajátot!</h4>
+      <h4 class="pt-5">Itt megtalálod az összes felhasználó által feltöltött képet, illetve a véleményeket!</h4>
     </div>
 </div>
 
 <?= $fullDivider ?>
 
 <div class="container">
-    <div class="row">
-        <div class="col-md-4 py-3">
-            <div class="card card-bg">
-                <img class="m-2 border-primary border-2 rounded-2 border-dark" src="https://backcountrygallery.com/wp-content/uploads/ngg_featured/peeking-raccoon.jpg" alt="Image 2">
-                <div class="card-body">
-                    <h5 class="card-title text-center">Image 2</h5>
-                    <hr style="border-width: 1px; border-color: black;" class="w-25 align-left">
-                </div>
+    <div class="row card-bg py-3 border my-3">
+        <div class="col-md-4 py-2 bg-light shadow mx-2">
+            <div class="d-flex flex-column justify-content-md-start justify-content-center align-items-center">
+                <img src="https://cc-prod.scene7.com/is/image/CCProdAuthor/adobe-firefly-marquee-text-to-image-0-mobile-600x600?$pjpeg$&jpegSize=100&wid=600" alt="" class="img-fluid border border-dark my-2 shadow"/>
+                <h6 class="card-title text-center py-3">Varga Dávid Zsolt Kecske</h6>
+            </div>
+        </div>
+        <div class="col-md-7 mx-4">
+            <div class="media-body">
+                <h5 class="card-text text-justify px-2 py-3">asdasd asdasd as ss dasd ffadfsdafsd f sdf sdf sdfs asdasd asdasd as ss dasd ffadfsdafsd f sdf sdf sdfs asdasd asdasd as ss dasd ffadfsdafsd f sdf sdf sdfs asdasd asdasd as ss dasd ffadfsdafsd f sdf sdf sdfs asdasd asdasd as ss dasd ffadfsdafsd f sdf sdf sdfs asdasd asdasd as ss dasd ffadfsdafsd f sdf sdf sdfs asdasd asdasd as ss dasd ffadfsdafsd f sdf sdf sdfs asdasd asdasd as ss dasd ffadfsdafsd f sdf sdf sdfs asdasd asdasd as ss dasd ffadfsdafsd f sdf sdf sdfs asdasd asdasd as ss dasd ffadfsdafsd f sdf sdf sdfs asdasd asdasd as ss dasd ffadfsdafsd f sdf sdf sdfs asdasd asdasd as ss dasd ffadfsdafsd f sdf sdf sdfs </h5>
             </div>
         </div>
     </div>
-    <!-- Add more rows for additional images -->
+    <div class="row card-bg py-3 border my-3">
+        <div class="col-md-4 py-2 bg-light shadow mx-2">
+            <div class="d-flex flex-column justify-content-md-start justify-content-center align-items-center">
+                <img src="https://cc-prod.scene7.com/is/image/CCProdAuthor/adobe-firefly-marquee-text-to-image-0-mobile-600x600?$pjpeg$&jpegSize=100&wid=600" alt="" class="img-fluid border border-dark my-2 shadow"/>
+                <h6 class="card-title text-center py-3">Varga Dávid Zsolt Kecske</h6>
+            </div>
+        </div>
+        <div class="col-md-7 mx-4">
+            <div class="media-body">
+                <h5 class="card-text text-justify px-2 py-3">asdasd asdasd as ss dasd ffadfsdafsd f sdf sdf sdfs asdasd asdasd as ss dasd ffadfsdafsd f sdf sdf sdfs asdasd asdasd as ss dasd ffadfsdafsd f sdf sdf sdfs asdasd asdasd as ss dasd ffadfsdafsd f sdf sdf sdfs asdasd asdasd as ss dasd ffadfsdafsd f sdf sdf sdfs asdasd asdasd as ss dasd ffadfsdafsd f sdf sdf sdfs asdasd asdasd as ss dasd ffadfsdafsd f sdf sdf sdfs asdasd asdasd as ss dasd ffadfsdafsd f sdf sdf sdfs asdasd asdasd as ss dasd ffadfsdafsd f sdf sdf sdfs asdasd asdasd as ss dasd ffadfsdafsd f sdf sdf sdfs asdasd asdasd as ss dasd ffadfsdafsd f sdf sdf sdfs asdasd asdasd as ss dasd ffadfsdafsd f sdf sdf sdfs </h5>
+            </div>
+        </div>
+    </div>
 </div>
 
-<div class="container text-center">
+<?= $fullDivider ?>
+
+<div class="container text-center" style="padding-top: 50px; padding-bottom: 50px;">
     <div class="card card-bg">
         <div class="card-body">
-            <h5 class="card-title">Upload Image</h5>
+            <h2 class="card-title">Írd meg a véleményed</h2>
             <form action="gallery.php" method="post" enctype="multipart/form-data">
-                <div class="form-group">
-                    <label for="image">Select Image:</label>
-                    <input type="file" class="form-control-file" name="image" id="image">
+                <div class="d-flex justify-content-center">
+                    <div class="form-group w-100 text-left">
+                        <label for="name">Név:</label>
+                        <input type="text" class="form-control" id="name" name="name" required>
+                    </div>
                 </div>
-                <button type="submit" class="btn btn-primary">Upload</button>
+                <div class="d-flex justify-content-center">
+                    <div class="form-group w-100 text-left">
+                        <label for="email">Email:</label>
+                        <input type="email" class="form-control" id="email" name="email" required>
+                    </div>
+                </div>
+                <div class="d-flex justify-content-center">
+                    <div class="form-group w-100 text-left">
+                        <label for="opinion">Vélemény:</label>
+                        <textarea class="form-control" id="opinion" name="opinion" rows="3" required></textarea>
+                    </div>
+                </div>
+                <div class="d-flex justify-content-center">
+                    <div class="input-group custom-file-button w-100">
+                        <label class="input-group-text card-bg" for="inputGroupFile">Kép választása</label>
+                        <input type="file" class="form-control" id="inputGroupFile" name="image">
+                    </div>
+                </div>
+                <div class="form-check py-3">
+                    <input type="checkbox" class="form-check-input" id="checkbox">
+                    <label class="form-check-label" for="checkbox">Elfogadom a szabályzatot</label>
+                </div>
+                <button type="submit" class="btn btn-primary w-50">Beküldés</button>
             </form>
         </div>
     </div>
