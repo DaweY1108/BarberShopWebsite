5abcb99f438667be0f6ef40474e5fd729c447c10
DaweY1108 <vargadavidzsolt@gmail.com>
2024-04-19 18:37:17 +0200

    barbers page redesign

--- pages/barbers.php
+++ pages/barbers.php
@@ -15,40 +15,30 @@
 
         <?= $fullDivider ?>
 
-        <div class="container">
-            <div class="row card-bg py-3 border my-3">
-                <div class="col-md-2 py-2 bg-light shadow mx-2">
-                    <div class="d-flex flex-column justify-content-md-start justify-content-center align-items-center">
-                        <img src="https://scontent-vie1-1.xx.fbcdn.net/v/t39.30808-6/344537323_257642919969993_6377759125325121225_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=5f2048&_nc_ohc=zfxsYzc2q00Ab4cIkFj&_nc_ht=scontent-vie1-1.xx&oh=00_AfAt-ugdzHDisXtMQwH64nXDjnhI5PA4BGTGsdEKH0Xfnw&oe=66282B7E" alt="" class="img-fluid border border-dark my-2 shadow"/>
-                        <h6 class="card-title text-center py-3">Antal Milán</h6>
-                    </div>
-                </div>
-                <div class="col-md-9 mx-4">
-                    <div class="media-body">
-                        <h4 class="mt-4">Bemutatkozás</h4>
-                        <hr style="border-width: 1px; border-color: black" class="w-100">
-                        <h6 class="mb-5">Sziasztok</h6>
-                        <h4 class="my-4">Skillek</h4>
-                        <hr style="border-width: 1px; border-color: black" class="w-100">
-                        <h6 class="mb-5">Nem tudom</h6>
-                    </div>
-                </div>
-            </div>
-            <div class="row card-bg py-3 border my-3">
-                <div class="col-md-2 py-2 bg-light shadow mx-2">
-                    <div class="d-flex flex-column justify-content-md-start justify-content-center align-items-center">
-                        <img src="https://scontent-vie1-1.xx.fbcdn.net/v/t39.30808-6/344537323_257642919969993_6377759125325121225_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=5f2048&_nc_ohc=zfxsYzc2q00Ab4cIkFj&_nc_ht=scontent-vie1-1.xx&oh=00_AfAt-ugdzHDisXtMQwH64nXDjnhI5PA4BGTGsdEKH0Xfnw&oe=66282B7E" alt="" class="img-fluid border border-dark my-2 shadow"/>
-                        <h6 class="card-title text-center py-3">Antal Milán</h6>
-                    </div>
-                </div>
-                <div class="col-md-9 mx-4">
-                    <div class="media-body">
-                        <h4 class="mt-4">Bemutatkozás</h4>
-                        <hr style="border-width: 1px; border-color: black" class="w-100">
-                        <h6 class="mb-5">Sziasztok</h6>
-                        <h4 class="my-4">Skillek</h4>
-                        <hr style="border-width: 1px; border-color: black" class="w-100">
-                        <h6 class="mb-5">Nem tudom</h6>
+        <div class='container mx-auto mt-5 col-12'>
+            <div class="row justify-content-center pb-5">
+                <div class="card card-bg col-md-6 mt-100">
+                    <div class="card-content">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <img src="https://marketplace.canva.com/EAFHfL_zPBk/1/0/1600w/canva-yellow-inspiration-modern-instagram-profile-picture-kpZhUIzCx_w.jpg" alt="Barber Image" class="img-fluid shadow border-1">
+                                </div>
+                                <div class="col-md-8">
+                                    <h4 class="text-center my-3">Kecske Béka</h4>
+                                    <h5>Bemutatkozás</h5>
+                                    <hr style="border-width: 1px; border-color: black">
+                                    <h6>Sziasztok gyelekik</h6>
+                                    <br>
+                                    <h5>Skillek</h5>
+                                    <hr style="border-width: 1px; border-color: black">
+                                    <h6>Nincs</h6>
+                                </div>
+                            </div>
+                            <div class="row justify-content-right pt-3">
+                                <button type="button" class="btn btn-lg btn-block btn-dark">Foglalok</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
