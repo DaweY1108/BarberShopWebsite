1a3745d319ff33adeeb3546f03918df1b9ca6c79
DaweY1108 <vargadavidzsolt@gmail.com>
2024-04-19 12:13:21 +0200

    Finished 404 error page

--- error.php
+++ error.php
@@ -2,9 +2,9 @@
 <html>
     <body>
         <div class="container d-flex flex-column justify-content-center align-items-center vh-100 py-lg-3">
-            <img src="assets/404.png" width="70%">
+            <img src="assets/404.png" class="png-shadow" width="70%">
             <h1 class="text-center">A keresett oldal nem található</h1>
-            <h2 class="text-center"=>Jó helyen jársz?</h2>
+            <h2 class="text-center">Jó helyen jársz?</h2>
         </div>
     </body>
 </html>
