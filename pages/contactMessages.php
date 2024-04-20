111170b802f144eaf0f6e04b9215f156bd21fc47
DaweY1108 <vargadavidzsolt@gmail.com>
2024-04-20 09:26:25 +0200

    Modified contact messages

--- pages/contactMessages.php
+++ pages/contactMessages.php
@@ -19,7 +19,7 @@
             <ul class="list-group">
                 <li class="list-group-item my-3 card-bg">
                     <div class="row">
-                        <div class="col-md-8">
+                        <div class="col-md-7">
                             <h4><i class="fa-solid fa-user mr-3"></i> John Doe</h4>
                             <hr style="border-width: 1px; border-color: black" class="w-100">
                             <h6 text-justify>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h6>
@@ -33,10 +33,11 @@
                                 <div class="form-group">
                                     <button type="submit" class="btn btn-dark w-100">Küldés</button>
                                 </div>
-                                <div class="form-group">
-                                    <button type="submit" class="btn btn-dark w-100">Törlés</button>
-                                </div>
                             </form>
+                            
+                        </div>
+                        <div class="col-md-1 d-flex justify-content-center align-items-center">
+                            <button class="btn btn-danger"><i class="fa-solid fa-x"></i></button>
                         </div>
                     </div>
                 </li>
