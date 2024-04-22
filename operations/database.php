29e7c5affc9fc321e47556e5ef6cebdb2a783546
DaweY1108 <vargadavidzsolt@gmail.com>
2024-04-22 20:09:36 +0200

    Test commit

--- operations/database.php
+++ operations/database.php
@@ -1,6 +1,6 @@
 <?php
     include(__DIR__ . '/../config.php');
-    $conn = new PDO("mysql:host=".$database['host'].";dbname=".$database['database'], $database['username'], $database['password']);
+    $conn = new PDO("mysql:host=".$database['host'].";dbname=".$database['database'], $database['username'], $database['password']); //Csatlakozas az adatbazishoz
     if (!$conn) {
         die("Fatal Error: Connection Failed!");
     }
