f9f14023e7687626e9dc265fa5bdfcfe29f49002
dawey1108 <vargadavidzsolt@gmail.com>
2024-04-25 17:13:20 +0200

    Api request now only work with post method

--- api/get_services.php
+++ api/get_services.php
@@ -1,9 +1,12 @@
 <?php
-    include('../operations/database.php');
-    header('Content-Type: application/json');
-    $stmt = $conn->prepare("SELECT * FROM services");
-    $stmt->execute();
-    $dataCount = $stmt->rowCount();
-    $fetchedData = $stmt->fetchAll();  
-    echo json_encode($fetchedData);
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        include('../operations/database.php');
+        header('Content-Type: application/json');
+        $stmt = $conn->prepare("SELECT * FROM services");
+        $stmt->execute();
+        $fetchedData = $stmt->fetchAll();
+        echo json_encode($fetchedData);
+    } else {
+        header('Location: ../index.php');
+    }
 ?>
